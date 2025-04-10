62315716cd0be6811880a8685004e90b5b437dab
Ashic KH <ashiq.hassan1995@gmail.com>
2025-04-10 11:55:27 +0530

    Snippet Delete API

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('snippets/create/', SnippetCreateAPIView.as_view(), name='snippet-create'),
     path('snippets/<int:pk>/', SnippetDetailAPIView.as_view(), name='snippet-detail'),
     path('snippets/<int:pk>/update/', SnippetUpdateAPIView.as_view(), name='snippet-update'),
+    path('snippets/<int:pk>/delete/', SnippetDeleteAPIView.as_view(), name='snippet-delete'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,11 @@
 from rest_framework.views import APIView
-from rest_framework.generics import CreateAPIView, RetrieveAPIView, RetrieveUpdateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Snippet
 from rest_framework.exceptions import PermissionDenied
 from .serializers import *
+from rest_framework import status
 
 class SnippetOverviewAPIView(APIView):
     permission_classes = [IsAuthenticated]
@@ -38,7 +39,6 @@ class SnippetDetailAPIView(RetrieveAPIView):
         return obj
 
 
-
 class SnippetUpdateAPIView(RetrieveUpdateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetUpdateSerializer
@@ -52,3 +52,23 @@ class SnippetUpdateAPIView(RetrieveUpdateAPIView):
 
     def get_serializer_context(self):
         return {'request': self.request}
+
+
+
+class SnippetDeleteAPIView(DestroyAPIView):
+    queryset = Snippet.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        obj = super().get_object()
+        if obj.created_by != self.request.user:
+            raise PermissionDenied("You do not have permission to delete this snippet.")
+        return obj
+
+    def delete(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        snippet.delete()
+
+        remaining_snippets = Snippet.objects.filter(created_by=request.user)
+        data = SnippetDetailSerializer(remaining_snippets, many=True).data
+        return Response(data, status=status.HTTP_200_OK)
